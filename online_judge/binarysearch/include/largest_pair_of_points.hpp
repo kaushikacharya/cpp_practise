2ca8d3d903e95943640a6ce140ca5c9727248f22
Kaushik Acharya <acharya.kaushik@gmail.com>
2021-02-01 11:11:14 +0530

    Largest Pair of Points

---
+++ online_judge/binarysearch/include/largest_pair_of_points.hpp
@@ -0,0 +1,10 @@
+#ifndef LARGEST_PAIR_OF_POINTS_HPP
+#define LARGEST_PAIR_OF_POINTS_HPP
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(vector<int>& nums, vector<int>& values);
+
+#endif // LARGEST_PAIR_OF_POINTS_HPP
